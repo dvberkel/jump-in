22f7b1a1b70b406364ceb71b8a6e4c171973a475
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-16 15:52:52 +0200

    Test annotation based prototype scope

--- context/src/main/java/nl/dvberkel/demo/annotation/AnswerProvider.java
+++ context/src/main/java/nl/dvberkel/demo/annotation/AnswerProvider.java
@@ -2,9 +2,11 @@ package nl.dvberkel.demo.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class AnswerProvider {
     @Autowired
     @Value("42")
--- context/src/test/java/nl/dvberkel/demo/AnnotationApplicationContextTest.java
+++ context/src/test/java/nl/dvberkel/demo/AnnotationApplicationContextTest.java
@@ -1,11 +1,13 @@
 package nl.dvberkel.demo;
 
+import nl.dvberkel.demo.annotation.AnswerProvider;
 import nl.dvberkel.demo.annotation.DeepThought;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class AnnotationApplicationContextTest {
@@ -17,5 +19,12 @@ public class AnnotationApplicationContextTest {
 
         assertThat(computer.provideAnswer(), is(42));
 
+    }
+
+    @Test
+    public void shouldCreateAnswerProvideInPrototypeScope() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("test.annotation.application-context.xml");
+
+        assertThat(context.getBean(AnswerProvider.class), is(not(context.getBean(AnswerProvider.class))));
     }
 }
